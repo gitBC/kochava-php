eb9c4e649bfe51db9cc795d6443d2ca1b1ffdbff
Brian Crist <bcrist@yetanothertech.com>
2017-09-01 10:59:59 -0700

    Creates delivery_log table to store statistics

---
+++ database/migrations/2017_09_01_173324_create_delivery_log_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeliveryLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('delivery_log', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('original_redis_key');
+            $table->string('delivery_method');
+            $table->text('delivery_location');
+            $table->smallInteger('delivery_attempts');
+            $table->integer('delivery_time_seconds');
+            $table->text('response_body');
+            $table->smallInteger('response_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('delivery_log');
+    }
+}
